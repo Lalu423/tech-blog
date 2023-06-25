92381152977b12d9a090a59c073ae2dde9b8f451
Lalu423 <>
2023-06-25 19:24:53 -0400

    added login code and routes

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require('./User');
+const Post = require('./Post');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+    foreign_key: 'user_id'
+});
+
+module.exports = { User, Post };
