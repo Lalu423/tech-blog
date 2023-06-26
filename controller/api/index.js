21f3dfad62b07dffeed68c81eab8c96e118f5e15
Lalu423 <>
2023-06-26 17:10:54 -0400

    added comment and post routes

--- controller/api/index.js
+++ controller/api/index.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
+
+
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
 const commentRoutes = requires('./commentRoutes');
 
-router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
-router.use('/posts', postRoutes);
+
+router.use('/user', userRoutes);
+router.use('/post', postRoutes);
+router.use('/comment', CommentRoutes);
 
 module.exports = router;
