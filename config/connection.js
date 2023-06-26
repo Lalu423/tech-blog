54cdcf6805322c16cd5223cc85f2c9a0c2847003
Lalu423 <>
2023-06-26 16:37:54 -0400

    fixed the connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,13 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
 
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-    sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        prcoess.env.DB_PASSWORD,
-        {
-            host: 'localhost',
-            dialect: 'mysql',
-            port: 3001
-        }
-    );
-}
+const sequelize = process.env.JAWSDB_URL
+? new Sequelize(process.env.JAWSDB_URL)
+: new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3001
+});
 
 module.exports = sequelize;
--- controller/api/index.js
+++ controller/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
+const commentRoutes = requires('./commentRoutes');
 
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 router.use('/posts', postRoutes);
 
 module.exports = router;
