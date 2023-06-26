5ac00bf23f8f854b2277e1a48ff013efee953f06
Lalu423 <>
2023-06-25 23:04:13 -0400

    created index model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
 
 
 
@@ -13,10 +14,24 @@ Post.belongsTo(User, {
     onDelete: 'CASCADE'
 });
 
+Comment.belongsTo(User, {
+    foerignkKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(Post, {
+    foerignKey: 'post_id',
+    onDelete: 'SET NULL'
+});
+
+User.hasMany(Comment, {
+    foereignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Post.hasMany(Comment, {
+    foerignKey: 'post_id'
+});
 
 
-
-
-
-
-module.exports = { User, Post };
+module.exports = { User, Post, Comment }
